0aec34a9d71b2cc1e35c03945bf059042d41eedf
GD3 <GD3@DESKTOP-TQ15C6N>
2019-04-23 11:46:47 +0900

    0423 1차 수정

---
+++ src/com/Y0423/Var.java
@@ -0,0 +1,39 @@
+package com.Y0423;
+
+public class Var {
+
+	public Var() {} //기본 생성자
+
+//리턴유형  함수명(매계변수) 로직 구현
+// void   var()       {}	
+	
+	public static void var() {
+		
+		/*******************************************
+		 *변수 사용법
+		 *1. 변수 선언
+		 *2. 변수에 데이터 대입(값담기)
+		 *3. 변수 사용 
+		 *******************************************/
+		int a;  //1
+		a = 1;   //2
+		System.out.println(a);  //3
+		
+		int b = 2;  // 1,2 동시 적용
+		System.out.println(b); //3
+		
+		int c, d, e; // 한번에 여러개 선언 가능 1
+		c = a;  //2  변수의 값으로 변수를 담을때  = c가 a의 주소값을 공유함
+		d = b;  //2 
+		e = c + d;  //2
+		System.out.println(e); //3
+		
+		int f;
+		f = a;
+		System.out.println(f);
+		
+		a = 10; // 일반 자료형의 변수는 주소값 대신 담았을때의 값만 받기 때문에 a의 값을 변경해도 f의 값은 변경되지 않음. 
+		System.out.println(a);
+		System.out.println(f);
+	}
+}
